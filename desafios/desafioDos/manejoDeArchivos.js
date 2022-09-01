f69889b4dab150e3e258f8d671b40359a744e7c6
David De Carli <josedaviddc@gmail.com>
2022-08-31 21:07:58 -0300

    Haciendo el segundo desafío

---
+++ desafios/desafioDos/manejoDeArchivos.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+
+class Contenedor{
+    constructor(name){
+        this.name = name;
+    }
+
+    async save (informacion){
+        try{
+            let contenido = await fs.promises.readFile(`./desafios/desafioDos/${this.name}`, 'utf-8');
+            let contenidoJson = JSON.parse(contenido);
+            let ultimoIndice = contenidoJson.lenght - 1;
+            let ultimoId = contenidoJson[ultimoIndice].id;
+            informacion.id = ultimoId + 1;
+            let id = informacion.id;
+            contenidoJson.push(informacion);
+            await fs.promises.writeFile(`./desafios/desafioDos/${this.name}`, JSON.stringify(contenidoJson));
+            return id;
+        }
+        catch(error){
+            console.log(error);
+        }
+    };
+
+    async getById(id){
+        try{
+            let contenidoDos = await fs.promises.readFile(`./desafios/desafioDos/${this.name}`, 'utf-8');
+            let contenidoDosJson = JSON.parse(contenidoDos);
+            let contenidoExtraidoDelArray
+            contenidoDosJson.forEach(element => {
+                if(element.id == id){
+                    contenidoExtraidoDelArray = element;
+                }
+            });
+            return contenidoExtraidoDelArray;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async getAll(){
+        let contenidoTres = await fs.promises.readFile(`./desafios/desafioDos/${this.name}`, 'utf-8');
+        let contenidoTresJson = JSON.parse(contenidoTres);
+        return contenidoTresJson;
+    }
+
+    deleteById(id){
+        try{
+            let contenidoCuatro = await fs.promises.readFile(`./desafios/desafioDos/${this.name}`, 'utf-8');
+            let contenidoCuatroJson = JSON.parse(contenidoCuatro);
+            let contenidoExtraidoDelArray
+            contenidoCuatroJson.forEach(element => {
+                if(element.id == id){
+                    contenidoExtraidoDelArray = element -1;
+                }
+            });
+            return contenidoExtraidoDelArray;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    deleteAll(){
+        let contenidoCinco = await fs.promises.readFile(`./desafios/desafioDos/${this.name}`, 'utf-8');
+        let contenidoCincoJson = JSON.parse([]);
+        contenidoCincoJson.push(contenidoCinco);
+        await fs.promises.writeFile(`./desafios/desafioDos/${this.name}`, JSON.stringify(contenidoJson));
+        return contenidoCincoJson;
+    }
+}
+
+let contenedor = new Contenedor("productos.json")
+
+let informacionNueva = {
+        "id": 4,
+        "title": "Tijeras",
+        "price": 213.76
+    }
+
+contenedor.save(informacionNueva).then(res =>{
+    console.log(res);
+});
+
+contenedor.getById(2).then(result =>{
+    console.log(result);
+});
+
+contenedor.getAll().then(resultado =>{
+    console.log(resultado);
+});
+
+contenedor.deleteById(2).then(re =>{
+    console.log(re);
+});
+
+contenedor.deleteAll().then(r =>{
+    console.log(r);
+});
---
+++ indexDos.js
@@ -0,0 +1,12 @@
+let gus = {
+    nick: "Wuisti",
+    promedioPeliculasMensuales: 5,
+    plataforma: "Netflix"
+};
+
+let ariel = {
+    nick: "Ari",
+    promedioPeliculasMensuales: 10,
+    plataforma: "Armazon"
+};
+
