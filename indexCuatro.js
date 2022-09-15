7e5e7a543cc7a3f121bd3150959784fa71422a53
David De Carli <josedaviddc@gmail.com>
2022-09-15 00:30:52 -0300

    Haciendo corrección al desafío 4

--- indexCuatro.js
+++ indexCuatro.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const apiRoutes = require('./desafios/desafioCuatro/routers/app.routers');
 
@@ -7,6 +8,23 @@ const PORT = process.env.PORT || 8080;
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./desafios/desafioCuatro/public'));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './desafios/desafioCuatro/public/index.html'));
+// });
+
+// app.get('/styles.css', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './desafios/desafioCuatro/public/styles.css'));
+// });
+
+// app.get('/browser-app.js', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './desafios/desafioCuatro/public/browser-app.js'));
+// });
+
+// app.get('/logo.svg', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './desafios/desafioCuatro/public/logo.svg'));
+// });
 
 //Routes
 app.use('/api', apiRoutes);
